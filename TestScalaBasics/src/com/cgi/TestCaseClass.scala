e136088d5bfd9925b091247992016c1a94804dde
guillaume gallois <guillaume.gallois@gmail.com>
2016-03-23 11:13:01 +0100

    add apply case class test

--- TestScalaBasics/src/com/cgi/TestCaseClass.scala
+++ TestScalaBasics/src/com/cgi/TestCaseClass.scala
@@ -4,12 +4,17 @@ package com.cgi
  * @author galloisg
  */
 object TestCaseClass extends App {
-  
+
   // case class are by default immutable
   val m = Model2("data1", "data2")
   println(m.copy(m.data1, m.data2, List("1", "2")))
-  
+
   val user = User(2, "Johanna", "Doe", 30, Some("meuf"))
   val parent = ParentModel("data1", user)
   println(s"parent : ${parent}")
+
+  // apply
+  val u = (User.apply _) (2, "Johanna", "Doe", 30, Some("meuf"))
+  println(s"u apply:${(User.apply _)}")
+  println(s"u:${u}")
 }
