ba86d2d324572b9cde5ccfc23fefd76f27511ada
guillaume gallois <guillaume.gallois@gmail.com>
2016-03-31 18:12:27 +0200

    add unapply test

--- TestScalaBasics/src/com/cgi/TestCaseClass.scala
+++ TestScalaBasics/src/com/cgi/TestCaseClass.scala
@@ -13,8 +13,20 @@ object TestCaseClass extends App {
   val parent = ParentModel("data1", user)
   println(s"parent : ${parent}")
 
-  // apply
+  // apply and tupled
   val u = (User.apply _) (2, "Johanna", "Doe", 30, Some("meuf"))
+  // follow this method with `_' if you want to treat it as a partially applied function
   println(s"u apply:${(User.apply _)}")
   println(s"u:${u}")
+  println(s"u apply bis:${User.apply(2, "Johanna", "Doe", 30, Some("meuf"))}")
+  
+  val tupleU = (2, "Johanna", "Doe", 30, Some("meuf"))
+  val ut = (User.apply _).tupled(tupleU)
+  println(s"ut apply:${(User.apply _).tupled}")
+  println(s"ut:${ut}")
+  
+  // unapply (extractors)
+  // http://danielwestheide.com/blog/2012/11/21/the-neophytes-guide-to-scala-part-1-extractors.html
+  println(s"unapply:${(User.unapply(User(2, "Johanna", "Doe", 30, Some("meuf"))))}")
+      
 }
