5b6eeccdee2bc89428ed80bf782f4747e9e34912
guillaume gallois <guillaume.gallois@gmail.com>
2017-04-06 23:00:51 +0200

    add links and map examples

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Another Scala basics tests, **not a sbt projet**, juste some simple scala objects (use for instance http://scala-ide.org/ for running them)
 
 lib folder for dependencies
+
+## links
+
+ - Nice [examples](https://github.com/loicknuchel/scala-class) at [DEVOXX 2017](http://cfp.devoxx.fr/2017/talk/XPI-0919/Scala_class,_bien_demarrer_avec_Scala), see [solutions](https://github.com/loicknuchel/scala-class/tree/master/src/test/scala/exercices)
--- TestScalaBasics/src/com/cgi/TestMap.scala
+++ TestScalaBasics/src/com/cgi/TestMap.scala
@@ -6,6 +6,8 @@ package com.cgi
 object TestMap extends App {
   
   // http://alvinalexander.com/scala/how-to-filter-map-filterkeys-retain-scala-cookbook
+  // https://www.scala-lang.org/api/current/scala/collection/Map.html
+  // Also others examples for : https://github.com/loicknuchel/scala-class
   
   val x = Map(1 -> "a", 2 -> "b", 3 -> "c")
   println(s"x:$x")
@@ -14,5 +16,14 @@ object TestMap extends App {
   println(s"y:$y")
   
   println(x.filter((t) => t._2 == "c"))
+  
+  val countries = Map("fr" -> "France", "be" -> "Belgique", "en" -> "Angleterre")
+  
+  println(countries.get("fr")) //Some("France")
+  
+  println(countries.map(country => (country._1, country._2.length))) // Map("fr" -> 6, "be" -> 8, "en" -> 10)
+  println(countries.map { case (code, name) => (code, name.length) }) // Map("fr" -> 6, "be" -> 8, "en" -> 10) // en utilisant le pattern matching \o/
+  println(countries.filter(_._2.contains("r"))) // Map("fr" -> "France", "en" -> "Angleterre")
+  println(countries.find(_._2.contains("e"))) // Some("fr", "France")
   
 }
